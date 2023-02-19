52339d9be5d6b3007f9828929511eb09106039ef
Lungisani <83877814+lungis4ni@users.noreply.github.com>
2023-02-19 09:45:27 +0200

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - Our main function of type int
+ * main - our main function of type int
  *
  * Return: 0
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
