d8705a9a372ca0795c60d546ec4ec8fd6010a52c
Lungisani <83877814+lungis4ni@users.noreply.github.com>
2023-02-19 09:23:41 +0200

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-int main() {
-    puts("\"Programming is like building a multilingual puzzle");
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle"); /* Using the puts function to print out */
     return 0;
 }
